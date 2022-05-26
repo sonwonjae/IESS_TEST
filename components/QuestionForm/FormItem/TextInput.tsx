ee3984cacd0c5da23f3f9da40399bc554bd557b1
sonwonjae <sonwj091552@gmail.com>
2022-05-27 02:40:47 +0900

    feat: QuestionForm에 사용될 Input 컴포넌트 구현 - #9

---
+++ components/QuestionForm/FormItem/TextInput.tsx
@@ -0,0 +1,59 @@
+import {
+  ChangeEventHandler,
+  memo,
+  PropsWithChildren,
+  SetStateAction,
+} from 'react';
+
+import * as FormItemStyled from './FormItem.style';
+
+interface TextInputProps {
+  id: string;
+  type?: 'text' | 'textarea';
+  placeholder: string;
+  required?: boolean;
+  value: string;
+  setValue: (value: SetStateAction<string>) => void;
+}
+
+function TextInput({
+  id,
+  type = 'text',
+  placeholder,
+  required = false,
+  value,
+  setValue,
+  children,
+}: PropsWithChildren<TextInputProps>) {
+  const changeValue: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <FormItemStyled.TextInputContainer>
+      {type === 'text' ? (
+        <input
+          id={id}
+          type="text"
+          placeholder={placeholder}
+          value={value}
+          required={required}
+          onChange={changeValue}
+        />
+      ) : (
+        <textarea
+          id={id}
+          placeholder={placeholder}
+          value={value}
+          required={required}
+          onChange={changeValue}
+        />
+      )}
+      <label htmlFor={id}>{children}</label>
+    </FormItemStyled.TextInputContainer>
+  );
+}
+
+export default memo(TextInput);
---
+++ components/QuestionForm/FormItem/index.ts
@@ -0,0 +1,3 @@
+export { default as TextInput } from './TextInput';
+export { default as ToggleInput } from './ToggleInput';
+export { default as HintInput } from './HintInput';
