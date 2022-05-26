2e861ba7e08d3f3137346d987ffa5bc621dd1752
sonwonjae <sonwj091552@gmail.com>
2022-05-27 02:38:06 +0900

    feat: 기능 확인 위해 작성한 Home 로직 제거 - #9

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,7 @@
-import { useCreateQuestion } from '@api/queries/question';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
 const Home: NextPage = () => {
-  const createQuestionMutate = useCreateQuestion({
-    onSuccess: (res) => {
-      console.log({ res });
-    },
-  });
-
   return (
     <>
       <Head>
@@ -16,22 +9,6 @@ const Home: NextPage = () => {
       </Head>
       <>
         <h2>HOME</h2>
-        <button
-          onClick={() => {
-            createQuestionMutate.mutate({
-              question: '테스트용 질문 1',
-              answer: '',
-              hints: ['답변 힌트 1', '답변 힌트 2'],
-              isPublic: true,
-              withInterview: false,
-              group: 'none',
-              searchKeyword: ['테', '스', '트', '용', '질', '문', '1'],
-              likedUsers: [],
-            });
-          }}
-        >
-          질문 추가
-        </button>
       </>
     </>
   );
