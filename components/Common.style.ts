7a671afc93794dd244c3cc8643e21f2dbcf18eb8
sonwonjae <sonwj091552@gmail.com>
2022-05-27 02:39:01 +0900

    style: 공통으로 사용될 Button Styled Component 추가 - #12

---
+++ components/Common.style.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: block;
+  width: 100%;
+  padding: ${({ theme }) => theme.layout.padding.small};
+  background-color: ${({ theme }) => theme.color.white};
+  color: ${({ theme }) => theme.color.primary[500]};
+  border: 0.1rem solid ${({ theme }) => theme.color.primary[500]};
+  border-radius: ${({ theme }) => theme.layout.borderRadius.medium};
+  font-weight: bold;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.primary.shadow[20]};
+  }
+
+  &:active {
+    background-color: ${({ theme }) => theme.color.primary[500]};
+    color: ${({ theme }) => theme.color.white};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.color.gray[300]};
+    color: ${({ theme }) => theme.color.gray[500]};
+    border: 0.1rem solid ${({ theme }) => theme.color.gray[300]};
+  }
+`;
