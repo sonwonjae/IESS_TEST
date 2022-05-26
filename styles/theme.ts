4c8f94a883ebad2b0bef6665e55f5ecc6e805a72
sonwonjae <sonwj091552@gmail.com>
2022-05-27 02:33:36 +0900

    feat: theme값 추가 - #1

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,29 +1,55 @@
 const color = {
   primary: {
     500: '#011377',
+    shadow: {
+      20: '#01137720',
+      40: '#01137740',
+      60: '#01137760',
+      80: '#01137780',
+    },
   },
   gray: {
-    200: '#F0F0F0',
+    100: '#F0F0F0',
     300: '#E3E3E3',
-    400: '#C4C4C4',
     500: '#808080',
-    600: '#4A4A4A',
-    700: '#333333',
   },
   white: '#ffffff',
   black: '#333333',
   dim: '#00000040',
   error: '#DC3232',
+  required: '#DC3232',
   disabled: '#f2f3f4',
 };
 
 const layout = {
-  padding: {
+  margin: {
+    xSmall: '0.4rem',
+    small: '0.8rem',
+    mediumSmall: '1.4rem',
     medium: '2rem',
   },
+  padding: {
+    xSmall: '0.4rem',
+    small: '0.8rem',
+    medium: '2rem',
+    mediumLarge: '2.4rem',
+  },
   borderRadius: {
+    small: '0.4rem',
     medium: '0.8rem',
     large: '50rem',
+  },
+  fontSize: {
+    small: '0.8rem',
+    mediumSmall: '1.2rem',
+    medium: '1.4rem',
+    mediumLarge: '1.8rem',
+  },
+  labelSize: '1.6rem',
+  checkbox: {
+    empty: '0.4rem',
+    checked: '0.8rem',
+    blank: '1.6rem',
   },
 };
 
