0992f1e45a8650c01e61a9f27b8e59ad8716732b
sonwonjae <sonwj091552@gmail.com>
2022-05-27 02:37:15 +0900

    feat: question list 확인하기 전 경로인 notes 페이지 추가 - #9

---
+++ pages/notes/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Notes: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>IESS | note list</title>
+      </Head>
+      <>
+        <h2>NOTES</h2>
+      </>
+    </>
+  );
+};
+
+export default Notes;
