3f6905a8f929ef33251da30d948e7258c21a5068
sonwonjae <sonwj091552@gmail.com>
2022-05-28 02:10:32 +0900

    feat: questions 타입 재정의 - #9

--- types/questions.d.ts
+++ types/questions.d.ts
@@ -8,6 +8,38 @@ interface Question {
   searchKeyword?: string[];
   likedUsers?: string[];
   interviewId?: string;
+  questionId?: string;
 }
 
 type Questions = Question[];
+
+interface GroupOrderMap {
+  [groupName: string]: number;
+}
+
+interface OrdersMap {
+  [groupName: string]: Questions;
+}
+
+interface QuestionsOrderMap {
+  [groupName: string]: {
+    [questionId: string]: number;
+  };
+}
+
+interface QuestionsOrder {
+  groupOrder: string[];
+  orders: {
+    [groupName: string]: string[];
+  };
+}
+
+type ResQuestionsData = [string, Questions][];
+
+interface ReqQuestion extends QuestionsOrder {
+  question: Question;
+}
+
+interface ResQuestions extends QuestionsOrder {
+  questions: ResQuestionsData;
+}
