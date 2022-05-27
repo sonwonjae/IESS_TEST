8e57b2d2ec80aee719117460f41e4fc571a2c3ab
sonwonjae <sonwj091552@gmail.com>
2022-05-27 18:41:50 +0900

    feat: interview GET api 구현

---
+++ pages/api/interviews.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { db } from '@firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Interviews>
+) {
+  const { method, cookies } = req;
+
+  const collectionId = 'interviews';
+  const interviewsRef = collection(db, collectionId);
+  const interviewsQuery = query(interviewsRef, where('uid', '==', cookies.uid));
+
+  switch (method) {
+    case 'GET':
+      const resBody: Interviews = [];
+      const interviewsQuerySnpshot = await getDocs(interviewsQuery);
+      interviewsQuerySnpshot.forEach((doc) => {
+        const data = {
+          ...(doc.data() as Interview),
+          interviewId: doc.id,
+        };
+        resBody.push(data);
+      });
+      res.status(200).json(resBody);
+      break;
+    default:
+      res.setHeader('Allow', ['POST', 'PUT', 'DELETE']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+}
